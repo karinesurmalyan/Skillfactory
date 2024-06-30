102bfc7ddc28e752b863ecc163c810c6ee7541d7
karinesurmalyan <91992115+karinesurmalyan@users.noreply.github.com>
2024-06-30 23:19:08 +0300

    19

--- Callboard/Callboard/board/signals.py
+++ Callboard/Callboard/board/signals.py
@@ -1,6 +1,5 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from .tasks import *
 from .models import Comment
 
@@ -11,10 +10,3 @@ def new_reply_signal(instance, created, **kwargs):
         reply_id = Comment.objects.get(id=instance.id).id
         new_comment_email_task.delay(reply_id)
 
-
-@receiver(pre_save, sender=Comment)
-def status_upd_signal(instance, **kwargs):
-    pre_state = Comment.objects.get(id=instance.id)
-
-    if not pre_state.is_accepted and instance.is_accepted:
-        status_upd_email_task.delay(instance.id)
